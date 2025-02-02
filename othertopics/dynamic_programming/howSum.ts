3329b924c033c464e3018aa1ef3e99e27bcd73b2
rshzrh <rshzrh@gmail.com>
2025-02-02 10:16:28 -0800

    how sum?

---
+++ othertopics/dynamic_programming/howSum.ts
@@ -0,0 +1,29 @@
+
+
+function howSum(targetSum: number, numbers:number[], memo: {} = {}): number[] | null{
+    // console.log(`${targetSum}`)
+    if(targetSum in memo)
+        return memo[targetSum]
+
+    if(targetSum === 0)
+        return []
+    if(targetSum < 0)
+        return null
+
+    for(let num of numbers){
+        const remainder = targetSum - num
+        const subNumbers = howSum(remainder, numbers, memo)
+        if(subNumbers !== null){
+            memo[targetSum] = [...subNumbers, num]
+            return memo[targetSum]
+        }
+    }
+    
+    memo[targetSum] = null    
+    return null
+}
+
+console.log(howSum(7, [5, 3, 4, 7]))
+console.log(howSum(8, [5, 3, 2]))
+console.log(howSum(7, [4, 2]))
+console.log(howSum(300, [14, 7]))
