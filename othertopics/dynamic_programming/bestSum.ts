13602893bcf0475941eee4f2e778069f01b9dabf
rshzrh <rshzrh@gmail.com>
2025-02-02 20:38:57 -0800

    best sum tabulation

--- othertopics/dynamic_programming/bestSum.ts
+++ othertopics/dynamic_programming/bestSum.ts
@@ -17,8 +17,41 @@ function bestSum(targetSum: number, numbers: number[]): number[] | null {
 
 }
 
-console.log(bestSum(7, [5, 3, 4, 7]))
-console.log(bestSum(8, [1, 4, 5]))
-console.log(bestSum(8, [2, 3, 5]))
-console.log(bestSum(7, [4, 2]))
-console.log(bestSum(100, [1, 2, 5, 25]))
+function bestSumTable(targetSum: number, numbers: number[]): number[] {
+    if(targetSum === 0)
+        return []
+
+    const table = new Array(targetSum + 1).fill(null)
+    table[0] = []
+    for(let num of numbers){
+        if(num < table.length){
+            table[num] = [num]
+        }
+    }
+
+    for(let i = 0; i < table.length; i++){
+        if(table[i] === null)
+            continue
+        for(let num of numbers){
+            if(i + num < table.length){
+                const combination = [...table[i], num]
+
+                if(table[i + num] === null){
+                    table[i + num] = combination
+                }else{
+                    if(table[i + num].length > combination.length)
+                        table[i + num] = combination
+                }
+            }
+        }
+    }
+    return table[targetSum]
+
+}
+
+console.log('------------------')
+console.log(bestSumTable(7, [5, 3, 4, 7]))
+console.log(bestSumTable(8, [1, 4, 5]))
+console.log(bestSumTable(8, [2, 3, 5]))
+console.log(bestSumTable(7, [4, 2]))
+console.log(bestSumTable(100, [1, 2, 5, 25]))
