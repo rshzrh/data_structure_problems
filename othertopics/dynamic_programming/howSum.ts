e4ff12ed38f1856130f4f1dbb5adc1a2a28f1b57
rshzrh <rshzrh@gmail.com>
2025-02-02 20:21:57 -0800

    how sum tabulation

--- othertopics/dynamic_programming/howSum.ts
+++ othertopics/dynamic_programming/howSum.ts
@@ -23,7 +23,35 @@ export function howSum(targetSum: number, numbers:number[], memo: {} = {}): numb
     return null
 }
 
-// console.log(howSum(7, [5, 3, 4, 7]))
-// console.log(howSum(8, [5, 3, 2]))
+function howSumTable(targetSum: number, numbers: number[]): number[]{
+    if(targetSum === 0)
+        return []
+
+    const table = new Array(targetSum + 1).fill(null)
+    table[0] = []
+    for(let num of numbers){
+        console.log(`processing ${num}`)
+        if(num < table.length){
+            table[num] = [num]
+        }
+    }
+    // consol e.log(table)
+    for(let i = 0; i < table.length; i++){
+        for(let num of numbers){
+            if(i + num < table.length){
+                if(table[i] === null)
+                    continue
+                
+                table[i + num] = [num, ...table[i]]
+            }
+        }
+    }
+    console.log(table)
+    return table[targetSum]
+
+}
+
+console.log(howSumTable(7, [5, 3, 4, 7]))
+// console.log(howSumTable(8, [5, 3, 2]))
 // console.log(howSum(7, [4, 2]))
 // console.log(howSum(300, [14, 7]))
