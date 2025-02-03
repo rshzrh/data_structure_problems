fd2395ef276ed3ed13996e6ad6b9baa7f38475b9
rshzrh <rshzrh@gmail.com>
2025-02-02 18:07:04 -0800

    can sum tabulation

--- othertopics/dynamic_programming/cansum.ts
+++ othertopics/dynamic_programming/cansum.ts
@@ -19,9 +19,33 @@ function canSum(targetSum:number, numbers: number[], memo={}): boolean{
     }
     memo[targetSum] = false
     return false
-
 }
-console.log(canSum(7, [5, 3, 4, 7]))
-console.log(canSum(7, [2, 3]))
-console.log(canSum(7, [2, 4]))
-console.log(canSum(300, [7, 14]))
+
+function canSumTable(targetSum: number, numbers: number[]): boolean{
+    if(targetSum ===  0)
+        return true
+
+    const table: boolean[] = new Array<boolean>(targetSum + 1).fill(false)
+    table[0] = true
+    for(let i = 1; i < table.length; i++){
+        if(numbers.includes(i)){
+            console.log(`i found: ${i}`)
+            table[i] = true
+        }
+            
+    }
+    console.log(table)    
+    for(let i = 1; i < table.length; i++){
+        for(let j = 0; j < numbers.length; j++){
+            if(numbers[j] < table.length)
+                table[i] = table[i] && table[numbers[j]]
+        }
+    }
+    console.log(table)
+    return table[targetSum]
+}
+
+console.log(canSumTable(7, [5, 3, 4, 7]))
+// console.log(canSum(7, [2, 3]))
+// console.log(canSum(7, [2, 4]))
+console.log(canSumTable(300, [7, 14]))
