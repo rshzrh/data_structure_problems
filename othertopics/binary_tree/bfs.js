c3388d389cae717acb53040d8ee35d91050ea1c1
rshzrh <rshzrh@gmail.com>
2025-02-05 08:58:20 -0800

    breadth first search

---
+++ othertopics/binary_tree/bfs.js
@@ -0,0 +1,49 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const breadthFirstValues = (root) => {
+  let queue = [];
+  const result = [];
+  if(root === null)
+    return result
+
+  queue.push(root)
+  while(queue.length > 0){
+    
+    const current = queue.shift()
+    result.push(current.val)
+    if(current.left != null)
+        queue.push(current.left)
+    if(current.right != null)
+        queue.push(current.right)
+
+  }
+
+  return result;
+};
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//      a
+//    /   \
+//   b     c
+//  / \     \
+// d   e     f
+
+console.log(breadthFirstValues(a)); //    -> ['a', 'b', 'c', 'd', 'e', 'f']
